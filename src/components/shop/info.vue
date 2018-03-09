7b57467bafe8fbe433f25ddf9579fed77abc8124
myue <657297833@qq.com>
2018-03-09 20:43:07 +0800

    second

--- src/components/shop/info.vue
+++ src/components/shop/info.vue
@@ -9,37 +9,58 @@
     </div>
 
     <div class="mui-card">
-      <!--页眉，放置标题-->
-      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-header">{{goodsInfo.title}}</div>
       <!--内容区-->
-      <div class="mui-card-content">内容区</div>
+      <div class="mui-card-content">
+         <div class="mui-card-content-inner">
+           <p class="price">
+             市场价：<del>￥{{goodsInfo.market_price}}</del>&nbsp;&nbsp;
+             销售价：<span class="now_price">￥{{goodsInfo.sell_price}}</span>
+           </p>
+           <p>购买数量：<number :quantity="goodsInfo.stock_quantity"></number> </p>
+           <p>
+             <mt-button type="primary" size="small">立即购买</mt-button>
+             <mt-button type="danger" size="small">加入购物车</mt-button>  
+           </p>
+         </div>
+      </div>
     </div>
 
     <div class="mui-card">
-      <!--页眉，放置标题-->
-      <div class="mui-card-header">页眉</div>
-      <!--内容区-->
-      <div class="mui-card-content">内容区</div>
-      <!--页脚，放置补充信息或支持的操作-->
-      <div class="mui-card-footer">页脚</div>
+      <div class="mui-card-header">商品参数</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">
+          <p>商品货号：{{goodsInfo.goods_no}}</p>
+          <p>库存情况：{{goodsInfo.stock_quantity}}件</p>
+          <p>上架时间：{{goodsInfo.add_time | dataF}}</p>
+        </div>
+      </div>
+      <div class="mui-card-footer">
+        <mt-button type="primary" size="large" plain>图文介绍</mt-button>
+        <mt-button type="danger" size="large" plain>商品评论</mt-button>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import swiper from '../subcom/swiper.vue';
+import number from '../subcom/number.vue';
 
 export default {
   data() {
     return {
       imgData:[],
-      id:this.$route.params.id
+      id:this.$route.params.id,
+      goodsInfo:[]
     };
   },
   components:{
-    swiper
+    swiper,
+    number
   },
   created(){
     this.getLunBo();
+    this.getGoodsInfo();
   },
   methods:{
     getLunBo(){
@@ -47,17 +68,37 @@ export default {
       .then((res)=>{
         if(res.body.status === 0 ){
           this.imgData = res.body.message;
-          console.log(this.imgData)
+          // console.log(this.imgData)
         }
       });
+    },
+    getGoodsInfo(){
+      this.$http.get('api/goods/getinfo/' + this.id)
+      .then((res)=>{
+        if(res.body.status ===0){
+          this.goodsInfo = res.body.message[0];
+          console.log(this.goodsInfo);
+        }
+      })
     }
   }
 }
 </script>
-<style lang="css" scoped>
+<style lang="less" scoped>
   .shopinfo-box{
     background-color: #eee;
     overflow: hidden;
     min-height: 500px;
+    .now_price{
+      color: red;
+      font-size: 16px;
+      font-weight: bold;
+    }
+    .mui-card-footer{
+      display: block;
+      button{
+        margin: 15px 0;
+      }
+    }
   }
 </style>
---
+++ src/components/subcom/number.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="mui-numbox" data-numbox-min="1" data-numbox-max="999">
+      <button class="mui-btn mui-btn-numbox-minus" type="button">-</button>
+      <input id="test" class="mui-input-numbox" type="number" value="1">
+      <button class="mui-btn mui-btn-numbox-plus" type="button">+</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  }
+}
+</script>
+<style lang="css" scoped>
+  .mui-numbox{
+      height: 30px;
+  }
+</style>
